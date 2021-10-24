070c89c5f214fb836513147a9c02ba078cd9f32d
mohamedveron <mohamedveron23@gmail.com>
2021-10-24 23:54:54 +0400

    add copy and paste event

--- domains/Event.go
+++ domains/Event.go
@@ -4,6 +4,8 @@ package domains
 type Event struct {
 	WebsiteUrl			string			`json:"websiteUrl"`
 	SessionId			string 			`json:"sessionId"`
+	EventType  			string 			`json:"eventType"`
+	TimeTaken  			int 			`json:"timeTaken"`
 	FormCompletionTime  int  			`json:"formCompletionTime"`
 	CopyAndPaste       	map[string]bool `json:"longitude_deg"`
 	ResizeFrom			Dimension  		`json:"resizeFrom"`

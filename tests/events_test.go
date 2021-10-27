b6d404d3e9568bf9f4916075d789446f0b91afc5
mohamedveron <mohamedveron23@gmail.com>
2021-10-28 03:22:07 +0400

    add test cases

--- tests/events_test.go
+++ tests/events_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"github.com/mohamedveron/events_detector/domains"
 	"github.com/mohamedveron/events_detector/service"
+	"sync"
 	"testing"
 )
 
@@ -57,10 +58,80 @@ func TestTriggerTimeTakenEvent(t *testing.T) {
 
 }
 
-func assertStringNil(t *testing.T, actual string) {
-	if &actual != nil {
-		t.Errorf("Expected string to be nil but was %s", actual)
+func TestTriggerScreenResizeEvent(t *testing.T) {
+
+	eventRequest := domains.EventRequest{
+		WebsiteUrl: "https://www.ravelin.com/",
+		SessionId:  "5553wrwq5tgbvfr4",
+		EventType:  "timeTaken",
+		TimeTaken:  0,
+		FormId:     "inputCardNumber",
+		ResizeFrom: domains.Dimension{
+			Width: "234",
+			Height: "165",
+		},
+		ResizeTo:   domains.Dimension{
+			Width: "300",
+			Height: "200",
+		},
+		Pasted:     false,
 	}
+
+	serviceLayer := service.NewService()
+
+	eventResponse, err := serviceLayer.HandleScreenResizeEvent(eventRequest)
+
+	if err != nil {
+		t.Errorf("Can't trigger the event")
+	}
+
+	assertStringEquals(t, eventRequest.SessionId, eventResponse.SessionId)
+
+}
+
+func TestTriggerCopyPasteEventConcurrently(t *testing.T) {
+
+	eventRequest := domains.EventRequest{
+		WebsiteUrl: "https://www.ravelin.com/",
+		SessionId:  "5553wrwq5tgbvfr4",
+		EventType:  "copyAndPaste",
+		TimeTaken:  0,
+		FormId:     "inputCardNumber",
+		ResizeFrom: domains.Dimension{},
+		ResizeTo:   domains.Dimension{},
+		Pasted:     true,
+	}
+
+	eventRequest2 := domains.EventRequest{
+		WebsiteUrl: "https://www.ravelin.com/",
+		SessionId:  "5553wrwq5tgbvfr4",
+		EventType:  "copyAndPaste",
+		TimeTaken:  0,
+		FormId:     "inputCVV",
+		ResizeFrom: domains.Dimension{},
+		ResizeTo:   domains.Dimension{},
+		Pasted:     true,
+	}
+
+	serviceLayer := service.NewService()
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go serviceLayer.HandleCopyAndPasteEvent(eventRequest)
+	go serviceLayer.HandleCopyAndPasteEvent(eventRequest2)
+
+	wg.Wait()
+
+	eventRequest.TimeTaken = 180
+	eventResponse, err := serviceLayer.HandleFormSubmissionEvent(eventRequest)
+
+	if err != nil {
+		t.Errorf("Can't trigger the event")
+	}
+
+	assertStringEquals(t, eventRequest.SessionId, eventResponse.SessionId)
+
 }
 
 func assertStringEquals(t *testing.T, expected string, actual string) {
